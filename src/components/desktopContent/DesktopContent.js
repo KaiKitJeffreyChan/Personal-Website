7187dedbbb9684cf08ba8052774b780fdfa2543f
Kai Kit Jeffrey Chan <jeffreykkchan@gmail.com>
2025-01-14 16:43:19 -0500

    chore: Adding Resume and resume button

--- src/components/desktopContent/DesktopContent.js
+++ src/components/desktopContent/DesktopContent.js
@@ -25,6 +25,14 @@ const DesktopContent = () => {
           />
         </h5>
       </TypeWriterCss>
+      <ResumeButton
+        href="/path/to/your/resume.pdf"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="resume-button"
+      >
+        Resume
+      </ResumeButton>
     </div>
   );
 };
@@ -38,6 +46,28 @@ const Title = styled.h1`
     text-align: center;
     z-index: 100;
     display:block; */
+`;
+
+const ResumeButton = styled.div`
+  margin-top: 20px;
+  margin-left: 5%;
+  font-size: 16px;
+  height: 36px;
+  width: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  background-color: #333;
+  border: none;
+  border-radius: 2px;
+  text-decoration: none;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #555;
+  }
 `;
 
 const TypeWriterCss = styled.div`
